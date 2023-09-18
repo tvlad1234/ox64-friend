d95e2e98b1cb157b40e372d9c1f0ef57d48dd69d
tvlad1234 <vlad.tomoiaga@gmail.com>
2023-09-18 12:58:10 +0300

    allow the 2 uarts to have different baudrates

--- src/cdc_uart.c
+++ src/cdc_uart.c
@@ -34,7 +34,8 @@ static uint8_t rx_buf[CFG_TUD_CDC_RX_BUFSIZE];
 static uint8_t tx2_buf[CFG_TUD_CDC_TX_BUFSIZE];
 static uint8_t rx2_buf[CFG_TUD_CDC_RX_BUFSIZE];
 
-void cdc_uart_init(void) {
+void cdc_uart_init(void)
+{
     gpio_set_function(PICOPROBE_UART_TX, GPIO_FUNC_UART);
     gpio_set_function(PICOPROBE_UART_RX, GPIO_FUNC_UART);
     gpio_set_pulls(PICOPROBE_UART_TX, 1, 0);
@@ -47,28 +48,34 @@ void cdc_uart_init(void) {
     uart_init(PICOPROBE_UART2_INTERFACE, PICOPROBE_UART2_BAUDRATE);
 }
 
-void cdc_task(void) {
+void cdc_task(void)
+{
     static int was_connected = 0;
     uint rx_len = 0;
     uint rx2_len = 0;
 
     // Consume uart fifo regardless even if not connected
-    while (uart_is_readable(PICOPROBE_UART_INTERFACE) && (rx_len < sizeof(rx_buf))) {
+    while (uart_is_readable(PICOPROBE_UART_INTERFACE) && (rx_len < sizeof(rx_buf)))
+    {
         rx_buf[rx_len++] = uart_getc(PICOPROBE_UART_INTERFACE);
     }
     // Consume uart fifo regardless even if not connected
-    while (uart_is_readable(PICOPROBE_UART2_INTERFACE) && (rx2_len < sizeof(rx2_buf))) {
+    while (uart_is_readable(PICOPROBE_UART2_INTERFACE) && (rx2_len < sizeof(rx2_buf)))
+    {
         rx2_buf[rx2_len++] = uart_getc(PICOPROBE_UART2_INTERFACE);
     }
 
-    if (tud_ready()) {
+    if (tud_ready())
+    {
         was_connected = 1;
         int written = 0;
         /* Implicit overflow if we don't write all the bytes to the host.
          * Also throw away bytes if we can't write... */
-        if (rx_len) {
+        if (rx_len)
+        {
             written = MIN(tud_cdc_write_available(), rx_len);
-            if (written > 0) {
+            if (written > 0)
+            {
                 tud_cdc_write(rx_buf, written);
                 tud_cdc_write_flush();
             }
@@ -76,7 +83,8 @@ void cdc_task(void) {
 
         /* Reading from a firehose and writing to a FIFO. */
         size_t watermark = MIN(tud_cdc_available(), sizeof(tx_buf));
-        if (watermark > 0) {
+        if (watermark > 0)
+        {
             size_t tx_len;
             /* Batch up to half a FIFO of data - don't clog up on RX */
             watermark = MIN(watermark, 16);
@@ -86,9 +94,11 @@ void cdc_task(void) {
         written = 0;
         /* Implicit overflow if we don't write all the bytes to the host.
          * Also throw away bytes if we can't write... */
-        if (rx2_len) {
+        if (rx2_len)
+        {
             written = MIN(tud_cdc_n_write_available(1), rx2_len);
-            if (written > 0) {
+            if (written > 0)
+            {
                 tud_cdc_n_write(1, rx2_buf, written);
                 tud_cdc_n_write_flush(1);
             }
@@ -96,33 +106,34 @@ void cdc_task(void) {
 
         /* Reading from a firehose and writing to a FIFO. */
         watermark = MIN(tud_cdc_n_available(1), sizeof(tx2_buf));
-        if (watermark > 0) {
+        if (watermark > 0)
+        {
             size_t tx_len;
             /* Batch up to half a FIFO of data - don't clog up on RX */
             watermark = MIN(watermark, 16);
             tx_len = tud_cdc_n_read(1, tx2_buf, watermark);
             uart_write_blocking(PICOPROBE_UART2_INTERFACE, tx2_buf, tx_len);
         }
-    } else if (was_connected) {
+    }
+    else if (was_connected)
+    {
         tud_cdc_write_clear();
         tud_cdc_n_write_clear(1);
         was_connected = 0;
     }
 }
 
-void tud_cdc_line_coding_cb(uint8_t itf, cdc_line_coding_t const* line_coding) {
-    /* Set the tick thread interval to the amount of time it takes to
-     * fill up half a FIFO. Millis is too coarse for integer divide.
-     */
-    uint32_t micros = (1000 * 1000 * 16 * 10) / MAX(line_coding->bit_rate, 1);
-
-    uart_deinit(PICOPROBE_UART_INTERFACE);
-    uart_deinit(PICOPROBE_UART2_INTERFACE);
+void tud_cdc_line_coding_cb(uint8_t itf, cdc_line_coding_t const *line_coding)
+{
     tud_cdc_write_clear();
     tud_cdc_read_flush();
-    uart_init(PICOPROBE_UART_INTERFACE, line_coding->bit_rate);
-    uart_init(PICOPROBE_UART2_INTERFACE, line_coding->bit_rate);
+
+    if (itf == 0)
+        uart_set_baudrate(PICOPROBE_UART_INTERFACE, line_coding->bit_rate);
+    else if (itf == 1)
+        uart_set_baudrate(PICOPROBE_UART2_INTERFACE, line_coding->bit_rate);
 }
 
-void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) {
+void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts)
+{
 }
